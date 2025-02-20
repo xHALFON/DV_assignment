0681b6d18788a07559eea84bdcb394a47a46a31a
xHALFON <guyhalfon98@gmail.com>
2025-02-20 18:20:16 +0200

    feat(atm/views.py): Updated the return value of get_balance, deposit and withdraw

--- atm/views.py
+++ atm/views.py
@@ -20,7 +20,7 @@ def get_balance(request, user_id): # Get balance from user
     try:
         with lock: # Prevent race condition
             balance = users_balance.get(user_id,0) # Fetch the user balance
-            return JsonResponse({"user_id": user_id, "balance": balance, "dict": users_balance})
+            return JsonResponse({"user_id": user_id, "balance": balance})
     except Exception as e:
         return JsonResponse({"err": str(e)}, status=400)
 
@@ -41,7 +41,7 @@ def deposit(request, user_id): # Deposit to user
                 return JsonResponse({"err": "Invalid deposit amount"}, status=400)
             users_balance[user_id] = users_balance.get(user_id, 0) + amount # Update the dict to the new balance
             print(f"user {user_id} new balance: {users_balance[user_id]}")
-            return JsonResponse({"user_id": user_id, "new_balance": users_balance[user_id], "dict": users_balance})
+            return JsonResponse({"user_id": user_id, "new_balance": users_balance[user_id]})
 
     except (ValueError, json.JSONDecodeError):
         return JsonResponse({"err": "Invalid JSON data"}, status=400)
@@ -68,7 +68,7 @@ def withdraw(request, user_id): # Withdraw from user
 
             users_balance[user_id] -= amount # Update the dict to the new balance
             print(f"user {user_id} new balance: {users_balance[user_id]}")
-            return JsonResponse({"user_id": user_id, "new_balance": users_balance[user_id], "dict": users_balance})
+            return JsonResponse({"user_id": user_id, "new_balance": users_balance[user_id]})
 
     except (ValueError, json.JSONDecodeError):
         return JsonResponse({"err": "Invalid JSON data"}, status=400)
